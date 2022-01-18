631263e2750c25d7203a67a1e84c8ee6006f782c
Hunter Frost <hunter2996@gmail.com>
2022-01-18 14:42:54 -0700

    finished week 6 project

--- index1.js
+++ index1.js
@@ -1,115 +1,117 @@
-/* 
-new deck
-shuffle deck
-deal every other shuffled card to player hand and the other to cpu hand
-draw card from each hand
-if player card > cpu card, played cards go to player hand
-if cpu card > player card. played cards go to cpu hand
-if cpu hand or player hand = 0
-if cpu hand == deck.length
-
-*/
 
 class Deck{
     constructor(){
         this.cards = [];
-        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+        this.makeDeck();
+        this.shuffleDeck();
         
+    }
+
+    makeDeck(){
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        const ranks = [ ' 2' ,' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9', ' 10', ' Jack', ' Queen', ' King', ' Ace'];
+        const values = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
         for(let j = 0; j < suits.length; j++){
-            for(let k = 0; k < values.length; k++){
-                this.cards.push(`${values[k]} of ${suits[j]} `)
+            for(let k = 0; k < ranks.length; k++){
+                this.cards.push([values[k] ,`${ranks[k]} of ${suits[j]}`]);
             }
         }
-        for (let i = this.cards.length - 1; i > 0; i--){
+    }
+    shuffleDeck(){
+        
+        for (let i = this.cards.length - 1 ; i > 0; i--){
             let j = Math.floor(Math.random() * i);
             let temp = this.cards[i];
             this.cards[i] = this.cards[j];
             this.cards[j] = temp;
-        }
+        }   
     }
+    
 }
 
-        
 
-
-
-
-class WAR{
+class WAR {
     constructor(){
-        this.deck = new Deck();
-        this.hand = [];
-        this.cpuHand = [];
-        this.playedCards = [];
-
+        
+        this.deck = new Deck().cards;
+        this.player1Deck = [];
+        this.player2Deck = [];
+        this.player1Card = [];
+        this.player2Card = [];
+        this.dealCards();
+        this.playerPoints = 0;
+        this.player2Points = 0;
+        this.menu();
     }
 
-    start(){
-        this.shuffleDeck(this.deck);
-        let selection = this.warMainMenu();
-        while (selection != 0){
-            switch (selection) {
-                case '1':
-                    this.startGame();
-            
-                default:
-                    selection = 0;
-            }
-            selection = this.warMainMenu()
-        }
-    }
-    startGame(){
+    menu(){
+        
         let selection = this.warGameMenu();
             switch (selection) {
                 case '1':
-                    this.dealCards();
-                    break;
-                case '2':
                     this.drawCard();
-                    break;
-                case '3':
-                    this.restartGame();
                 default:
-                    selection = 0
+                    selection = this.restartGame();
                     
             }  
-            selection = this.warGameMenu
         }
-    warMainMenu(){
-        return prompt(`
-        0) Exit
-        1) Start game
-        `)
-    }
     
     warGameMenu(){
+        
         return prompt(`
-        0) Exit
-        1) Restart
-        2) Draw card
+        0) Restart
+        1) Draw card 
+        Player 1: ${this.playerPoints} -------War------- Player 2: ${this.player2Points}
+        Player 1 Deck : ${this.player1Deck.length} | Player 2 Deck : ${this.player2Deck.length}
+        Player 1 card: ${this.player1Card[1]}
+        Player 2 card: ${this.player2Card[1]} 
+        
         `)
     }
-
-    
     
     dealCards(){
-        
+            this.player1Deck = this.deck.splice(0, 26);
+            this.player2Deck = this.deck.splice(0, 26);
     }
-
-    
 
     restartGame(){
-        new WAR()
+        new WAR().menu();
         
     }
+    
     drawCard(){
+        this.player1Card = this.player1Deck.shift();
+        this.player2Card = this.player2Deck.shift();
+        this.winner();
+        this.menu();
 
     }
 
-    
-    
+    winner(){
+        if(this.player1Card[0] < this.player2Card[0]){
+            this.player2Deck.push(this.player1Card, this.player2Card)
+            this.player2Points++
+           }
+        else if(this.player1Card[0] > this.player2Card[0]){ 
+           this.player1Deck.push(this.player1Card, this.player2Card)
+           this.playerPoints++
+        }
+        else {
+            this.player1Deck.push(this.player1Card)
+            this.player2Deck.push(this.player2Card)
+            
+}
+       
+        if(this.playerPoints == 26){
+            return alert('Player 1 wins!')
+            
+        }if(this.player2Points == 26){
+            return alert('Player 2 Wins!')
+        }
+    }
+
 }
 
-let war = new WAR();
-war.start();
+new WAR();
+
 
